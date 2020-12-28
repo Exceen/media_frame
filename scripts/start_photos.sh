7a49766553a1b142b0c24cfc3fc72d46f509fbe9
Exceen <github@exceen.at>
2020-12-28 21:27:38 +0100

    disable mqtt for the artwork display

--- scripts/start_photos.sh
+++ scripts/start_photos.sh
@@ -3,7 +3,6 @@ export DISPLAY=:0.0;
 python /home/pi/scripts/github/media_frame/pi3d_demos/PictureFrame2020.py \
     --blur_edges 1 \
     --keyboard 1 \
-    --use_mqtt 1 \
     --blur_amount 5 \
     --background "(0, 0, 0, 1.0)" \
     --shuffle 1 \
@@ -13,5 +12,7 @@ python /home/pi/scripts/github/media_frame/pi3d_demos/PictureFrame2020.py \
     --show_text_tm 0 \
     --check_dir_tm 3600 \
     --show_text "" > /dev/null&
+
+    #--use_mqtt 1 \
 
 echo $!;
