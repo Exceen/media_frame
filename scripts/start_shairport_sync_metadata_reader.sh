e1b2708098fcd5bb5b88e5fb227577d2634a472e
Exceen <github@exceen.at>
2020-12-28 14:24:47 +0100

    updated service files

--- scripts/start_shairport_sync_metadata_reader.sh
+++ scripts/start_shairport_sync_metadata_reader.sh
@@ -2,4 +2,4 @@
 
 export DISPLAY=:0.0;
 #cat /tmp/shairport-sync-metadata | python3 ~/scripts/github/shairport-sync-metadata-python/bin/output_text.py
-cat /tmp/shairport-sync-metadata | python3 ~/scripts/github/media_frame/scripts/shairport_sync_pipe_reader.py
+cat /tmp/shairport-sync-metadata | python3 /home/pi/scripts/github/media_frame/scripts/shairport_sync_pipe_reader.py
