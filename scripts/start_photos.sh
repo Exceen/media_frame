c7055f4e2c9583a957a6b68aaba82a58588c480b
Exceen <github@exceen.at>
2021-01-01 02:13:57 +0100

    adjusted time_delay

--- scripts/start_photos.sh
+++ scripts/start_photos.sh
@@ -8,9 +8,9 @@ python /home/pi/scripts/github/media_frame/pi3d_demos/PictureFrame2020.py \
     --shuffle 1 \
     --pic_dir /home/pi/scripts/github/media_frame/data/photos \
     --fade_time 6 \
-    --time_delay 3600 \
+    --time_delay 600 \
     --show_text_tm 0 \
-    --check_dir_tm 3600 \
+    --check_dir_tm 1800 \
     --show_text "" > /dev/null&
 
     #--use_mqtt 1 \
